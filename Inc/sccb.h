bfc7ee559e51f5d6e66b3ae7b8c06c3f69fe8cb4
George Vigelette <gvigelet@duvitech.com>
2017-09-22 11:45:57 -0400

    added files for sccb work

---
+++ Inc/sccb.h
@@ -0,0 +1,13 @@
+/*
+ * sccb.h
+ *
+ *  Created on: Sep 22, 2017
+ *      Author: gvige
+ */
+
+#ifndef SCCB_H_
+#define SCCB_H_
+
+
+
+#endif /* SCCB_H_ */
---
+++ Src/sccb.c
@@ -0,0 +1,8 @@
+/*
+ * sccb.c
+ *
+ *  Created on: Sep 22, 2017
+ *      Author: gvige
+ */
+
+
